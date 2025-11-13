36821f2eae6bfd5678d9db7d827f910325fd76fb
Kai Renner <kai.renner@gmail.com>
2025-11-13 16:59:43 +0100

    Deploy: 8fbcedc at 2025-11-13T15:52:56.772Z
    Deployed from: main
    Build directory: dist
    Tests run: no

---
+++ chunks/load-view-transitions.Ci0Nrjyd.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/view-transitions.hqp4Lwk5.css"])))=>i.map(i=>d[i]);
+import{_ as e}from"./vendor-other.DbSZRnBI.js";let t=!1;async function o(){t||(await e(()=>Promise.resolve({}),__vite__mapDeps([0])),t=!0)}export{o as loadViewTransitions};
